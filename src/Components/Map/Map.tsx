9c533803057350867f868f322c606801214c3730
Armando <armandoc9943@gmail.com>
2021-02-17 19:36:48 -0500

    webpack config

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from "mapbox-gl";
 import "./Map.css";
 import { IInitialMap, IProvince, markers } from "../../data";
